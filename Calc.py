af78f38d05f31cd0ded475b2e11f932b0f4152ac
Christy-McChristy <chraar06@imkatta.no>
2026-03-20 15:02:51 +0100

    update!
    the Calc and first product done

---
+++ Calc.py
@@ -0,0 +1,4 @@
+number1 = input("enter a number: ")
+number2 = input("enter another number: ")
+
+print(f"the sum of {number1} and {number2} is: {int(number1) + int(number2)}")
---
+++ index.py
@@ -0,0 +1,6 @@
+
+
+print("hello world! hey you there! whatcha name, and hey, how old are ya?")
+name = input("what's your name? ")
+Age = input("How old are you?")
+print(f"why hello {name}! You are {Age} years old.")
