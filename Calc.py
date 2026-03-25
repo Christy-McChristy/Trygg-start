da15b1921d2aaf4d849e960aa51f3d9c09b41d8a
Christy-McChristy <chraar06@imkatta.no>
2026-03-25 08:31:03 +0100

    Calculator doneso

--- Calc.py
+++ Calc.py
@@ -1,4 +1,8 @@
 number1 = input("enter a number: ")
 number2 = input("enter another number: ")
 
-print(f"the sum of {number1} and {number2} is: {int(number1) + int(number2)}")
+number1 = int(number1)
+number2 = int(number2)
+
+result = number1 + number2
+print(f"your total number are {result}")
--- index.py
+++ index.py
@@ -2,5 +2,5 @@
 
 print("hello world! hey you there! whatcha name, and hey, how old are ya?")
 name = input("what's your name? ")
-Age = input("How old are you?")
-print(f"why hello {name}! You are {Age} years old.")
+age = input("How old are you?")
+print(f"why hello {name}! You are {age} years old.")
